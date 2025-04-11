e4522d8f3a5039227faa21459af1ced78d54dd4c
JohnGuerreroCorLait <john.guerrero@lait.com.co>
2025-04-10 20:30:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, WebSocket, HTTPException  # type: ignore
+from fastapi import FastAPI, Request, WebSocket, HTTPException  # type: ignore
 import httpx
 from fastapi.middleware.cors import CORSMiddleware  # type: ignore
 from fastapi.responses import JSONResponse  # type: ignore
